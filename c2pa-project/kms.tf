6f95f683fce2746a3fad84f168b2d2242dd58128
Toby Scales <17342265+tobyscales@users.noreply.github.com>
2025-11-21 15:08:30 -0800

    Update kms.tf

--- c2pa-project/kms.tf
+++ c2pa-project/kms.tf
@@ -15,7 +15,7 @@ resource "google_kms_crypto_key" "signing_key" {
   key_ring        = google_kms_key_ring.keyring.id
   purpose         = "ASYMMETRIC_SIGN"
   version_template {
-    algorithm        = "RSA_SIGN_PKCS1_4096_SHA256"
+    algorithm = "RSA_SIGN_PSS_2048_SHA256"
     protection_level = "HSM"
   }
 }
